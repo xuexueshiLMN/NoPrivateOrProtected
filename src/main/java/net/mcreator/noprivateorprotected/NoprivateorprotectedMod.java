4d26cb843de5d9480d136059035d632362145941
apphhzp <76092931@qq.com>
2023-06-18 13:19:52 +0800

    .

---
+++ src/main/java/net/mcreator/noprivateorprotected/NoprivateorprotectedMod.java
@@ -0,0 +1,8 @@
+package net.mcreator.noprivateorprotected;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("noprivateorprotected")
+public class NoprivateorprotectedMod {
+	public NoprivateorprotectedMod() {}
+}
