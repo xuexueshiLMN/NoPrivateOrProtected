a2f7a395e611b1da9f556413c9eb7d4f634ccb8d
apphhzp <76092931@qq.com>
2023-06-22 14:13:13 +0800

    .

--- src/main/java/net/mcreator/noprivateorprotected/NoprivateorprotectedMod.java
+++ src/main/java/net/mcreator/noprivateorprotected/NoprivateorprotectedMod.java
@@ -1,8 +1,11 @@
 package net.mcreator.noprivateorprotected;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("noprivateorprotected")
 public class NoprivateorprotectedMod {
-	public NoprivateorprotectedMod() {}
+	public NoprivateorprotectedMod() {
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
 }
